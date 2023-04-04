d73073eedb761f1551b44cb861f484682889e42b
LeeHongCl <113571786+LeeHongCl@users.noreply.github.com>
2023-04-04 11:28:34 +0900

    24 HTTP DELETE 를 이용한 Delete Rest API 개발

--- src/main/java/com/example/todo/service/TodoService.java
+++ src/main/java/com/example/todo/service/TodoService.java
@@ -60,6 +60,15 @@ public class TodoService {
 		return repository.findById(entity.getId());
 	}
 	
+	public String delete(final String id) {
+		if(repository.existsById(id)) {
+			repository.deleteById(id);
+		}else {
+			throw new RuntimeException("id dows not exist");
+		}
+		return "Deleted";
+	}
+	
 	public void validate(final TodoEntity entity) {
 		if(entity ==null) {
 			log.warn("Entity cannot be null.");
